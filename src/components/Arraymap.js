cd7b9de77f826f0a428f4101731f0fabbcac9944
Margarito Valencia III <52177176+MValenciaIII@users.noreply.github.com>
2024-10-30 14:11:29 -0500

    Update Arraymap.js

--- src/components/Arraymap.js
+++ src/components/Arraymap.js
@@ -1,6 +1,35 @@
 import React from "react";
 
-
+const fakeData = [
+    {
+        fname: "Scott",
+        lname: "Syverson",
+        transportation: "car",
+        age: 50,
+        old: false
+    },
+    {
+        fname: "Nicole",
+        lname: "Torres",
+        transportation: "car",
+        age: 32,
+        old: false
+    },
+    {
+        fname: "Caleb",
+        lname: "Straily",
+        transportation: "home",
+        age: 24,
+        old: false
+    },
+    {
+        fname: "Caleb",
+        lname: "Straily",
+        transportation: "home",
+        age: 24,
+        old: false
+    }
+]
 
 function Arraymap() {
 
@@ -27,6 +56,26 @@ function Arraymap() {
 
     console.log(capitalize);
 
+    const pokemon = ["Bulbasaur", "Charmander", "Squirtle"];
+
+    const pokemonPTags = pokemon.map(
+        (jimbo) => {
+            return <p>{jimbo}</p>
+        }
+    )
+    // const pokemonPTags = [<p>Bulbasaur</p>, <p>Charmander</p>, <p>Squirtle</p>];
+
+    //!!!!! Mapping out Components
+    const listComponents = fakeData.map(
+        (currentItem) => {
+            return <TempItem 
+            firstName={currentItem.fname} lastname={currentItem.lname}
+            transpo={currentItem.transportation}
+            ageNum={currentItem.age}
+            bool={currentItem.old} />
+        }
+    )
+
     return (
         <div className="row">
             <div className="col-12">
@@ -35,6 +84,24 @@ function Arraymap() {
             <div className="col-12">
                 {capitalize};
             </div>
+            <div className="col-12">
+                {pokemonPTags}
+            </div>
+            {listComponents}
+        </div>
+    )
+}
+
+
+function TempItem(props) {
+    console.log(props)
+
+    return (
+        <div className="col-3">
+            <p>First Name: {props.firstName} Last Name: {props.lastname}</p>
+            <p>Transportation: {props.transpo}</p>
+            <h1>AGE: {props.ageNum}</h1>
+            <h3></h3>
         </div>
     )
 }
